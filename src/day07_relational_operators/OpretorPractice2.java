ee12aad25fb03e659df87de25700b89ded3bf9ed
parvi <parviz.aghamir@gmail.com>
2023-09-23 03:32:53 +1000

    my new commit

---
+++ src/day07_relational_operators/OpretorPractice2.java
@@ -0,0 +1,21 @@
+package day07_relational_operators;
+
+public class OpretorPractice2 {
+
+    public static void main(String[] args) {
+
+        int a = 40;
+        System.out.println(--a);
+        System.out.println(--a);
+        System.out.println(a--);
+        System.out.println(--a);
+        System.out.println(--a);
+        System.out.println(a--);
+        System.out.println(a);
+
+
+
+
+
+    }
+}
---
+++ src/day07_relational_operators/UpdateVariable.java
@@ -0,0 +1,28 @@
+package day07_relational_operators;
+
+public class UpdateVariable {
+    public static void main(String[] args) {
+
+        int a =10;
+        a++;
+
+        a=a+1;
+
+
+        int b =4;
+        b= b+10;
+        b=4+10;
+        b+=10;
+        int count = 0;
+        count +=5;
+        count -=10;
+        count *=2;
+        System.out.println(a);
+        System.out.println(count);
+
+
+
+
+    }
+
+}
