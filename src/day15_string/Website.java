0f120267e4ff9f4e726d97d2be7108dbc5241f35
parvi <parviz.aghamir@gmail.com>
2023-10-01 06:10:26 +1100

    my new commit

---
+++ src/day15_string/Website.java
@@ -0,0 +1,28 @@
+package day15_string;
+
+import java.util.Scanner;
+
+public class Website {
+
+/*    Task
+
+        ask the user to enter a url
+        find and print the website from the url
+
+        assume your url always starts with www.
+        assume your url always ends with .com
+
+        www.google.com
+
+        --> google
+     */
+public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter a URL: ");
+    String url= input.next();
+
+
+}
+
+
+}
