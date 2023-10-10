8ece5a6ba9779fd5a07cc432d1b70aa7536d805f
parvi <parviz.aghamir@gmail.com>
2023-10-11 02:51:26 +1100

    Fix day7

---
+++ src/day07_relational_operators/OpretorPractice4.java
@@ -0,0 +1,14 @@
+package day07_relational_operators;
+
+public class OpretorPractice4 {
+
+    public static void main(String[] args) {
+        int t = 100;
+        int p = - ++t*-t--/t++ + --t;
+        System.out.println(p);
+    int c = 100 % 30;
+        System.out.println(c);
+
+
+    }
+}
