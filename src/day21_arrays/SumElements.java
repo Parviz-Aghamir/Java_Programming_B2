1f493f6b2660ff7768e2876c46b2fd3ff987ffc4
parvi <parviz.aghamir@gmail.com>
2023-10-16 00:34:28 +1100

    nested Loop

---
+++ src/day21_arrays/SumElements.java
@@ -0,0 +1,15 @@
+package day21_arrays;
+
+public class SumElements {
+    public static void main(String[] args) {
+        int[] num = {3,5,7,8};
+        int sum = 0;
+
+        for (int i = 0; i < num.length ; i++) {
+            //System.out.println(num[i]);
+            sum += num[i];
+        }
+        System.out.println("Sum of total: " + sum);
+    }
+
+}
